790040e7d890fc5c2492f00baecbe354625c1b13
Mikołaj Albrecht <mikson.alb@wp.pl>
2022-06-11 16:57:19 +0200

    dodanie tablicy do tasków

--- strona/src/components/App.js
+++ strona/src/components/App.js
@@ -5,11 +5,37 @@ import AddTask from './dodajZadanie';
 import TaskList from './listaZadan';
 
 class App extends Component {
+
+  state = {
+    tasks : [
+      {
+        id : 0,
+        message: 'zamówić wakacje',
+        date: '2022-06-30',
+        important: true,
+        active: true,
+      },
+      {
+        id : 1,
+        message: 'kupić prezent na urodziny',
+        date: '2022-06-30',
+        important: true,
+        active: true,
+      },
+      {
+        id : 2,
+        message: 'skosić trawę',
+        date: '2022-06-30',
+        important: false,
+        active: true,
+      }
+    ]
+  }
   render() {
     return(
       <div className='App'>
         <AddTask />
-        <TaskList />
+        <TaskList tasks={this.state.tasks}/>
 
       </div>
     )
--- strona/src/components/listaZadan.js
+++ strona/src/components/listaZadan.js
@@ -1,14 +1,11 @@
 import { Component } from "react"
 import Task from './zadanie';
-const TaskList = () => {
+const TaskList = (props) => {
+    const tasks = props.tasks.map(task => <Task key={task.id} task={task} />)
     return (
         <div>
-            <h3>Lista zadań do zrobienia</h3>
-            <Task />
-            <Task />
-            <Task />
-            <Task />
-            <Task />
+            <h3>Lista zadań do zrobienia:</h3>
+            {tasks}
         </div>
     )
 }
