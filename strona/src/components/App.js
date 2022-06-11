a2b09d5958b62e8dedf778b8e5d1b5bc09552857
Mikołaj Albrecht <mikson.alb@wp.pl>
2022-06-11 15:53:56 +0200

    pierwsza zmiana na stronie

---
+++ strona/src/components/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <p>Poprawiony komponent App</p>
+
+      </div>
+    )
+  }
+}
+
+export default App;
